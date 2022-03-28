30c16e067ec5fdfa3a6164ed13bf0061f77d1364
Eleryn Jäer <ejaer@tktk.ee>
2022-03-28 22:38:36 +0300

    directories and files, wishlists and shopping lists

---
+++ ShoppingList2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\samples\shoppingList";
+            string fileName = @"\\myShoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileName}");
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add something to your shopping list? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Add something to your list:");
+                    string userWish = Console.ReadLine();
+                    myShoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Have a nice day!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myShoppingList)
+            {
+                Console.WriteLine($"Your shopping list: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileName}", myShoppingList);
+        }
+    }
+}
